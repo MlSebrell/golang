c5b1732b690b33f839f247acd4a4138c5db2a5aa
Mcoy Sebrell <mlsebrell@gmail.com>
2017-01-05 14:18:20 -0500

    modyfied slices.go

--- slices.go
+++ slices.go
@@ -57,5 +57,23 @@ func main() {
 	
 	// and this slices up from (and including) `s[2]`.
 	l = s[2:]
-	fmt.Println
+	fmt.Println("sl3:", l)
+	
+	// we can declare and intialize a variable for slice
+	// in a single line as well.
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl:", t)
+	
+	// slices can be composed into multi-dimensional data
+	// structures. the length of the inner slices can
+	// var, unlike with multi-dimensional arrays.
+	twoD := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+		}
+	}
+	fmt.Println("2d: ", twoD)
+}
 	
